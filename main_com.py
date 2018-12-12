f8eb4afd6ddab1ce78dd0a0b80319832cf017918
Vandrande-Varg <41341852+Vandrande-Varg@users.noreply.github.com>
2018-12-12 17:12:49 +0800

    added alt colour detector

--- main_com.py
+++ main_com.py
@@ -6,48 +6,74 @@ import pickle
 import time
 import cv2
 import pyttsx3 as pyttsx
+import numpy as np
 
 from colorlabeler import ColorLabeler
 
+lower = {'red': (166, 84, 141),
+		 'green': (66, 122, 129),
+		 'blue': (97, 100, 117),
+		 'yellow': (23, 59, 119),
+		 'orange': (0, 50, 80)}  # assign new item lower['blue'] = (93, 10, 0)
+
+upper = {'red': (186, 255, 255),
+		 'green': (86, 255, 255),
+		 'blue': (117, 255, 255),
+		 'yellow': (54, 255, 255),
+		 'orange': (20, 255, 255)}
+
+colors = {'red': (0, 0, 255),
+		  'green': (0, 255, 0),
+		  'blue': (255, 0, 0),
+		  'yellow': (0, 255, 217),
+		  'orange': (0, 140, 255)}
+
 encoding_file = "encodings_2.pickle"
 cascade_file = "haarcascade_frontalface_default.xml"
+engine = pyttsx.init()
 
 # load the known faces and embeddings
 print("[INFO] loading encodings...")
 data = pickle.loads(open(encoding_file, "rb").read())
 detector = cv2.CascadeClassifier(cascade_file)
+engine.say("loading encodings")
+engine.runAndWait()
 
 # initialize the video stream and pointer to output video file, then
 # allow the camera sensor to warm up
 print("[INFO] starting video stream...")
-#vs = VideoStream(usePiCamera=True).start()
+# vs = VideoStream(usePiCamera=True).start()
 vs = VideoStream(src=1).start()
-engine = pyttsx.init()
-time.sleep(2)
+# camera = cv2.VideoCapture(1)
+engine.say("starting video stream")
+engine.runAndWait()
+
 try:
 	# loop over frames from the video file stream
 	while True:
 
 		# grab the frame from the threaded video stream
 		frame = vs.read()
+		# (grabbed, frame) = camera.read()
 		frame = imutils.resize(frame, width=500)
+		cv2.imshow("Frame", frame)
 
-		gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-		rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-		blurred = cv2.GaussianBlur(frame, (5, 5), 0)
-
-		#key = cv2.waitKey(1) & 0xFF
-		key = input("t or q")
-		#if key == ord("q"):
-		if key == ("q"):
+		key = cv2.waitKey(1) & 0xFF
+		# key = input("t or q")
+		if key == ord("q"):
+		# if key == "q":
 			break
-		#elif key == ord("t"):
-		elif key == ("t"):
+		elif key == ord("t"):
+		# elif key == "t":
 
 			print("Picture Taken")
 
+			gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+			rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
 			# detect faces in the grayscale frame
-			rects = detector.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+			rects = detector.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),
+											  flags=cv2.CASCADE_SCALE_IMAGE)
 
 			# OpenCV returns bounding box coordinates in (x, y, w, h) order
 			# but we need them in (top, right, bottom, left) order, so we
@@ -86,45 +112,74 @@ try:
 				engine.runAndWait()
 				names.append(name)
 
-		elif key == ("c"):
+		#elif key == "c":
+		elif key == ord("c"):
+
+			blurred = cv2.GaussianBlur(frame, (11, 11), 0)
 
 			image_height = blurred.shape[0]  # get image height
 			image_width = blurred.shape[1]  # get image width
-			ymid = int(image_height/2)
-			xmid = int(image_width/2)
+			ymid = int(image_height / 2)
+			xmid = int(image_width / 2)
 
-			x1 = xmid - 50
-			y1 = ymid + 50
+			size = 100
 
-			x2 = xmid + 50
-			y2 = ymid - 50
+			x1 = xmid - size
+			y1 = ymid + size
 
-			# initialize the color labeler
-			cl = ColorLabeler()
+			x2 = xmid + size
+			y2 = ymid - size
 
 			clone = blurred.copy()
-			crop_img = clone[y2:y1,x1:x2]
+			crop_img = clone[y2:y1, x1:x2]
 
+			"""
 			gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
 			lab = cv2.cvtColor(crop_img, cv2.COLOR_BGR2LAB)
 			thresh = cv2.threshold(gray, 60, 255, cv2.THRESH_BINARY)[1]
-
+			
 			cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 			cnts = imutils.grab_contours(cnts)
-
+			
+			cl = ColorLabeler()
+			
 			for c in cnts:
 				color = cl.label(lab, c)
 				print(color)
 				engine.say(color)
 				engine.runAndWait()
+			"""
+
+			hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
+
+			for key, value in upper.items():
+
+				kernel = np.ones((9, 9), np.uint8)
+				mask = cv2.inRange(hsv, lower[key], upper[key])
+				mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+				mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
+				cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+				# only proceed if at least one contour was found
+				if len(cnts) > 0:
+
+					c = max(cnts, key=cv2.contourArea)
+					((x, y), radius) = cv2.minEnclosingCircle(c)
 
 
-		cv2.imshow("Frame", frame)
+					if radius > 0.5:
+
+						print(key)
+						engine.say(key)
+						engine.runAndWait()
 
 	# do a bit of cleanup
 	cv2.destroyAllWindows()
 	vs.stop()
+	exit()
 
 except KeyboardInterrupt:
 	cv2.destroyAllWindows()
 	vs.stop()
+	exit()
